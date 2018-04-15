150dbc92e64d3c8867f22fa3a4964958bfd5fc73
arknoid <omalige@gmail.com>
2018-04-15 20:55:05 +0200

    change fonction name in english and fix color to dead and inventory in console

--- js/app.js
+++ js/app.js
@@ -1,98 +1,95 @@
 var app = {
-  ennemiesPool: [],
+  enemyPool: [],
 
   init: function() {
     app.start();
   },
 
   start: function() {
-    app.joueur = app.creationJoueur('arknoid', 100, 5, 10);
-    app.joueur.description();
-    app.joueur.afficheInventaire();
+    app.player = app.createPlayer('arknoid', 100, 5, 10);
+    app.player.describ();
+    app.player.afficheInventaire();
 
-    app.createEnnemiesPool(5);
-    app.ennemiesPool.forEach(function(enemies) {
-      while (enemies.sante > 0) {
-        app.joueur.combattre(enemies);
-        app.joueur.afficheInventaire();
+    app.createEnemysPool(5);
+    app.enemyPool.forEach(function(enemies) {
+      while (enemies.life > 0) {
+        app.player.combattre(enemies);
+        app.player.afficheInventaire();
       };
     });
   },
 
-  createEnnemiesPool : function (nombre) {
-    for (var index = 0 ; index < nombre; index++) {
-      app.ennemiesPool.push(app.creationEnemmie('ZogZog'+index, 6, 4, 'orc'));
+  createEnemysPool : function (namebre) {
+    for (var index = 0 ; index < namebre; index++) {
+      app.enemyPool.push(app.createEnemy('ZogZog'+index, 6, 4, 'orc'));
     }
   },
 
-  Ennemie: function(nom, sante, force, race) {
-    app.Personnage.call(this, nom, sante, force, race);
+  Enemy: function(name, life, strength, race) {
+    app.Character.call(this, name, life, strength, race);
     this.race = race;
-    this.description = function() {
-      console.log("je suis " + this.nom + 'je suis un : ' + this.race + ' j\'ai ' + this.sante + ' de sante et ' + this.force + ' de force.');
+    this.describ = function() {
+      console.log("je suis " + this.name + 'je suis un : ' + this.race + ' j\'ai ' + this.life + ' de life et ' + this.strength + ' de strength.');
     };
 
     //Definie la Valeur du nombre d 'experiences en cas d'élimination suivant la race
     if (this.race === 'orc') {
       this.valeur = 100;
-      this.or = 20;
+      this.gold = 20;
     };
   },
 
 
-  Personnage: function(nom, sante, force) {
-    this.nom = nom;
-    this.sante = sante;
-    this.force = force;
-    this.description = function() {
-      console.log("je suis " + this.nom + ' j\'ai ' + this.sante + ' de sante et ' + this.force + ' de force.');
+  Character: function(name, life, strength) {
+    this.name = name;
+    this.life = life;
+    this.strength = strength;
+    this.describ = function() {
+      console.log("je suis " + this.name + ' j\'ai ' + this.life + ' de life et ' + this.strength + ' de strength.');
     };
-    // Attaque un Ennemie cible
-    this.attaquer = function(cible) {
-      if (this.sante > 0) {
-        var degats = this.force;
-        console.log(this.nom + " attaque " + cible.nom + " et lui fait " + degats + " points de dégâts");
-        cible.sante = cible.sante - degats;
-        if (cible.sante > 0) {
-          console.log(cible.nom + " a encore " + cible.sante + " points de vie");
+    // Attaque un Enemy cible
+    this.attack = function(target) {
+      if (this.life > 0) {
+        var damage = this.strength;
+        console.log(this.name + " attaque " + target.name + " et lui fait " + damage + " points de dégâts");
+        target.life = target.life - damage;
+        if (target.life > 0) {
+          console.log(target.name + " a encore " + target.life + " points de vie");
         } else {
-          cible.sante = 0;
-          console.log(cible.nom + " est mort !");
+          target.life = 0;
+          console.error(target.name + " est mort !");
         }
-      } else {
-        ennemiesPool = [],
-          console.log(this.nom + " ne peut pas attaquer : il est mort...");
       }
     }
   },
 
-  Joueur: function(nom, sante, force, or) {
-    app.Personnage.call(this, nom, sante, force);
-    this.or = or;
+  Player: function(name, life, strength, gold) {
+    app.Character.call(this, name, life, strength);
+    this.gold = gold;
     this.xp = 0;
-    this.clef = 0;
+    this.key = 0;
     this.afficheInventaire = function() {
-      console.log('Inventaire : ' + this.or + " or " + this.clef + ' clef ' + this.xp + ' experiences ' + this.sante + ' points de vies');
+      console.warn('Inventory : ' + this.gold + " gold " + this.key + ' key ' + this.xp + ' experiences ' + this.life + ' lifes');
     };
-    this.combattre = function(adversaire) {
-      this.attaquer(adversaire);
-      adversaire.attaquer(this);
-      if (adversaire.sante === 0) {
-        console.log(this.nom + " a tué " + adversaire.nom + " gagne " +
-          adversaire.valeur + " points d'expérience" + ' et  ' + adversaire.or + ' d\'or');
-        this.xp += adversaire.valeur;
-        this.or += adversaire.or;
+    this.combattre = function(opponent) {
+      this.attack(opponent);
+      opponent.attack(this);
+      if (opponent.life === 0) {
+        console.log(this.name + " a tué " + opponent.name + " gagne " +
+          opponent.valeur + " points d'expérience" + ' et  ' + opponent.gold + ' d\'or');
+        this.xp += opponent.valeur;
+        this.gold += opponent.gold;
       }
     };
   },
 
-  creationJoueur: function(nom, sante, force, or) {
-    var joueur = new app.Joueur(nom, sante, force, or);
-    return joueur;
+  createPlayer: function(name, life, strength, gold) {
+    var player = new app.Player(name, life, strength, gold);
+    return player;
   },
 
-  creationEnemmie: function(nom, sante, force, race) {
-    var ennemie = new app.Ennemie(nom, sante, force, race);
+  createEnemy: function(name, life, strength, race) {
+    var ennemie = new app.Enemy(name, life, strength, race);
     return ennemie;
   }
 };
