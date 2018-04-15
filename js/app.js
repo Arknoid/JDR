27805506936c8efc231b6f4aa90ec005ef1dca7b
arknoid <omalige@gmail.com>
2018-04-15 14:45:20 +0200

    ajout des methodes attaquer et combattre

--- js/app.js
+++ js/app.js
@@ -2,18 +2,29 @@ var app = {
   init: function() {
     app.joueur = app.creationJoueur('arknoid', 100, 5, 10);
     app.joueur.description();
-    app.joueur.showOr();
-    app.orc = app.creationEnemmie('ZogZog', 30 , 4 , 'orc');
+    app.joueur.afficheInventaire();
+    app.orc = app.creationEnemmie('ZogZog', 30, 4, 'orc');
     app.orc.description();
-
+    
+    while (app.orc.sante > 0) {
+      app.joueur.combattre(app.orc);
+    }
+    app.joueur.afficheInventaire();
   },
   Ennemie: function(nom, sante, force, race) {
     app.Personnage.call(this, nom, sante, force, race);
     this.race = race;
     this.description = function() {
-      console.log("je suis " + this.nom + 'je suis un : '+this.race+' j\'ai ' + this.sante + ' de sante et ' + this.force + ' de force.');
+      console.log("je suis " + this.nom + 'je suis un : ' + this.race + ' j\'ai ' + this.sante + ' de sante et ' + this.force + ' de force.');
+    };
+
+    //Definie la Valeur du nombre d 'experiences en cas d'élimination suivant la race
+    if (this.race === 'orc') {
+      this.valeur = 100;
+      this.or = 20;
     };
   },
+
 
   Personnage: function(nom, sante, force) {
     this.nom = nom;
@@ -22,13 +33,41 @@ var app = {
     this.description = function() {
       console.log("je suis " + this.nom + ' j\'ai ' + this.sante + ' de sante et ' + this.force + ' de force.');
     };
+    // Attaque un Ennemie cible
+    this.attaquer = function(cible) {
+      if (this.sante > 0) {
+        var degats = this.force;
+        console.log(this.nom + " attaque " + cible.nom + " et lui fait " + degats + " points de dégâts");
+        cible.sante = cible.sante - degats;
+        if (cible.sante > 0) {
+          console.log(cible.nom + " a encore " + cible.sante + " points de vie");
+        } else {
+          cible.sante = 0;
+          console.log(cible.nom + " est mort !");
+        }
+      } else {
+        console.log(this.nom + " ne peut pas attaquer : il est mort...");
+      }
+    }
   },
 
   Joueur: function(nom, sante, force, or) {
     app.Personnage.call(this, nom, sante, force);
     this.or = or;
-    this.showOr = function() {
-      console.log(this.or + " or");
+    this.xp = 0;
+    this.clef = 0;
+    this.afficheInventaire = function() {
+      console.log( 'Inventaire : '+this.or + " or " + this.clef + ' clef ' + this.xp + ' experiences');
+    };
+    this.combattre = function(adversaire) {
+      this.attaquer(adversaire);
+      adversaire.attaquer(this);
+      if (adversaire.sante === 0) {
+        console.log(this.nom + " a tué " + adversaire.nom + " gagne " +
+          adversaire.valeur + " points d'expérience" + ' et  ' + adversaire.or + ' d\'or');
+        this.xp += adversaire.valeur;
+        this.or += adversaire.or;
+      }
     };
   },
 
@@ -38,8 +77,8 @@ var app = {
   },
 
   creationEnemmie: function(nom, sante, force, race) {
-     var ennemie = new app.Ennemie(nom ,sante , force , race);
-     return ennemie;
+    var ennemie = new app.Ennemie(nom, sante, force, race);
+    return ennemie;
   }
 };
 
