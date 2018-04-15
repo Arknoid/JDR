550bb48111b168a373a26607014171c2c291c8a1
arknoid <omalige@gmail.com>
2018-04-15 13:50:38 +0200

    test et mise en place du stystem d'objets avec heritage

--- js/app.js
+++ js/app.js
@@ -1,41 +1,49 @@
 var app = {
-  init : function() {
-    console.log("init");
-    var Personnage = {
-        // Initialise le personnage
-        initPerso: function (nom, sante, force) {
-            this.nom = nom;
-            this.sante = sante;
-            this.force = force;
-            this.or = 0;
-            this.clef = 0;
-        },
-        // Attaque un personnage cible
-        attaquer: function (cible) {
-            if (this.sante > 0) {
-                var degats = this.force;
-                console.log(this.nom + " attaque " + cible.nom + " et lui fait " + degats + " points de dégâts");
-                cible.sante = cible.sante - degats;
-                if (cible.sante > 0) {
-                    console.log(cible.nom + " a encore " + cible.sante + " points de vie");
-                } else {
-                    cible.sante = 0;
-                    console.log(cible.nom + " est mort !");
-                }
-            } else {
-                console.log(this.nom + " ne peut pas attaquer : il est mort...");
-            }
-        }
+  init: function() {
+    app.joueur = app.creationJoueur('arknoid', 100, 5, 10);
+    app.joueur.description();
+    app.joueur.showOr();
+    app.orc = app.creationEnemmie('ZogZog', 30 , 4 , 'orc');
+    app.orc.description();
+
+  },
+  Ennemie: function(nom, sante, force, race) {
+    app.Personnage.call(this, nom, sante, force, race);
+    this.race = race;
+    this.description = function() {
+      console.log("je suis " + this.nom + 'je suis un : '+this.race+' j\'ai ' + this.sante + ' de sante et ' + this.force + ' de force.');
     };
-
-
-  },
-  createPlayer:function() {
-
   },
 
+  Personnage: function(nom, sante, force) {
+    this.nom = nom;
+    this.sante = sante;
+    this.force = force;
+    this.description = function() {
+      console.log("je suis " + this.nom + ' j\'ai ' + this.sante + ' de sante et ' + this.force + ' de force.');
+    };
+  },
 
+  Joueur: function(nom, sante, force, or) {
+    app.Personnage.call(this, nom, sante, force);
+    this.or = or;
+    this.showOr = function() {
+      console.log(this.or + " or");
+    };
+  },
 
+  creationJoueur: function(nom, sante, force, or) {
+    var joueur = new app.Joueur(nom, sante, force, or);
+    return joueur;
+  },
+
+  creationEnemmie: function(nom, sante, force, race) {
+     var ennemie = new app.Ennemie(nom ,sante , force , race);
+     return ennemie;
+  }
 };
 
-document.addEventListener("DOMContentLoaded", app.init )
+
+
+
+document.addEventListener("DOMContentLoaded", app.init)
