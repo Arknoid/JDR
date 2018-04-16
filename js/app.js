f53550e590d1124a0e8d78692b0545ca0a552cbc
Arknoid <arknoid@ArchArknoid.localdomain>
2018-04-16 07:33:12 +0200

    additions weapons with random range damages and obj to generate player and enemy

--- js/app.js
+++ js/app.js
@@ -1,56 +1,82 @@
 var app = {
+
+  //tableau a remplir d'ennemie grace a createEnemysPool();
   enemyPool: [],
 
+  // obj pour cree le joueur (temporaire)
+  arknoid :{
+    name : 'arknoid',
+    life : 100,
+    strength :5,
+    dexterity : 15,
+    intelligence : 20,
+    gold :100
+  },
+
+  //enemie de base pour les testes
+  orc : {
+    name :'zogzog',
+    life : 20,
+    strength : 10,
+    dexterity : 10,
+    inteligence : 5,
+    race : 'orc'
+  },
+
+
   init: function() {
+    // debugger;
     app.start();
+
   },
 
+  //Demarage du jeu !
   start: function() {
-    app.player = app.createPlayer('arknoid', 100, 5, 10);
-    app.player.describ();
-    app.player.afficheInventaire();
 
+    //Apelle du constructeur du joueur  avec un objet contenan  "nom,points de vie,force,dexterité,inteligence,or"
+    app.player = app.createPlayer(app.arknoid);
+
+    //debug
+    app.player.showInventory();
+
+    //Generation d'ennemies
     app.createEnemysPool(5);
-    app.enemyPool.forEach(function(enemies) {
-      while (enemies.life > 0) {
-        app.player.combattre(enemies);
-        app.player.afficheInventaire();
-      };
-    });
+
+    //Boucle permetant de de continuer tant que le joueur n'est pas mort et que la pool d'ennemies n'est pas vide
+    while (app.player.life > 0 && app.enemyPool.length > 1) {
+      //Pour chaque enemies generer le joueur doit les combattres les uns apres les autres (pour le moment)
+      app.enemyPool.forEach(function(enemy) {
+        while (enemy.life > 0 && app.player.life > 0) {
+          app.player.combat(enemy);
+          //debug
+          app.player.showInventory();
+        };
+      });
+    };
+    app.gameOver();
   },
 
-  createEnemysPool : function (namebre) {
-    for (var index = 0 ; index < namebre; index++) {
-      app.enemyPool.push(app.createEnemy('ZogZog'+index, 6, 4, 'orc'));
-    }
+  //Generateur de nombres aleatoir arondie
+  randomNumber: function(min = 0, max = 0) {
+    return Math.round(Math.random() * (max - min) + min);
   },
 
-  Enemy: function(name, life, strength, race) {
-    app.Character.call(this, name, life, strength, race);
-    this.race = race;
+
+  //Prototype de Base ! qui peu evoluer vers 'player' ou  'enemy' grace au systeme d'heritage;
+  Character: function(obj) {
+    this.name = obj.name;
+    this.life = obj.life;
+    this.strength = obj.strength;
+    this.dexterity = obj.dexterity;
+    this.intelligence = obj.intelligence;
     this.describ = function() {
-      console.log("je suis " + this.name + 'je suis un : ' + this.race + ' j\'ai ' + this.life + ' de life et ' + this.strength + ' de strength.');
+      console.log("Je suis " + this.name + ' et j\'ai ' + this.life + ' de vies avec ' + this.strength + ' de strength ');
     };
-
-    //Definie la Valeur du nombre d 'experiences en cas d'élimination suivant la race
-    if (this.race === 'orc') {
-      this.valeur = 100;
-      this.gold = 20;
-    };
-  },
-
-
-  Character: function(name, life, strength) {
-    this.name = name;
-    this.life = life;
-    this.strength = strength;
-    this.describ = function() {
-      console.log("je suis " + this.name + ' j\'ai ' + this.life + ' de life et ' + this.strength + ' de strength.');
-    };
-    // Attaque un Enemy cible
+    // Attaque un enemie cible
     this.attack = function(target) {
       if (this.life > 0) {
-        var damage = this.strength;
+        //les degats sont calculer avec la force + un nombre aleatoir suivant la puissance min et max de l'arme du porteur
+        var damage = this.strength + app.randomNumber(this.weapon.damageMin,this.weapon.damageMax);
         console.log(this.name + " attaque " + target.name + " et lui fait " + damage + " points de dégâts");
         target.life = target.life - damage;
         if (target.life > 0) {
@@ -63,15 +89,42 @@ var app = {
     }
   },
 
-  Player: function(name, life, strength, gold) {
-    app.Character.call(this, name, life, strength);
-    this.gold = gold;
+  //Prototype pour les ennemies
+  Enemy: function(obj) {
+    app.Character.call(this,obj);
+    this.race = obj.race;
+    this.weapon = new app.items.weapons.Arms('Rusty sword',1,4);
+    //petite presentation pour debug
+    this.describ = function() {
+      console.log("je suis " + this.name + 'de race : ' + this.race + ', j\'ai ' + this.life + ' de vies  et ' + this.strength + ' de strength.');
+    };
+
+    //Definie la Valeur du nombre d 'experiences recus en cas d'élimination suivant la race
+    if (this.race === 'orc') {
+      this.valeur = 100;
+      this.gold = 20;
+    };
+  },
+
+
+  //Prototype de creation du joueur
+  Player: function(obj) {
+    //Apelle du construteur du Prototype de base 'Charactere'
+    app.Character.call(this, obj);
+    //Plus des proprietés spécifique
+    this.gold = obj.gold;
     this.xp = 0;
     this.key = 0;
-    this.afficheInventaire = function() {
-      console.warn('Inventory : ' + this.gold + " gold " + this.key + ' key ' + this.xp + ' experiences ' + this.life + ' lifes');
+    // Arme de base choisie dans le Namespace items.weapons
+    this.weapon = new app.items.weapons.Arms('Axe',4,6);
+
+    //Affiche inventaire pour debug
+    this.showInventory = function() {
+      console.warn('Inventory : ' + this.gold + " gold " + this.key + ' key ' + this.xp + ' experiences ' + this.life + ' lifes ' + 'arms : ' + this.weapon.name);
     };
-    this.combattre = function(opponent) {
+
+    this.combat = function(opponent) {
+      //Character.attack()
       this.attack(opponent);
       opponent.attack(this);
       if (opponent.life === 0) {
@@ -83,14 +136,50 @@ var app = {
     };
   },
 
-  createPlayer: function(name, life, strength, gold) {
-    var player = new app.Player(name, life, strength, gold);
+  //Fonction pour crée le joueur basé sur le Prototype charactere -> player (!!!Voué a etre remplacer pour le DOM!! )
+  createPlayer: function(obj) {
+    var player = new app.Player(obj);
     return player;
   },
 
-  createEnemy: function(name, life, strength, race) {
-    var ennemie = new app.Enemy(name, life, strength, race);
+  //Fonction pour crée un enemie  basé sur le Prototype charactere -> Enemy (!!!Voué a etre remplacer dans le DOM!! )
+  createEnemy: function(obj) {
+    var ennemie = new app.Enemy(obj);
     return ennemie;
+  },
+
+  //Permet de definir le nombre d'ennemies en lice
+  createEnemysPool: function(numberToAdd) {
+    for (var index = 0; index < numberToAdd; index++) {
+      app.enemyPool.push(app.createEnemy(app.orc));
+    }
+  },
+
+  gameOver: function(){
+    console.error("You are Dead");
+    console.error("Game Over");
+  },
+
+  //NameSpace pour les items
+  items: {
+    weapons: {
+      Arms : function(name,damageMin,damageMax) {
+          this.name = name;
+          this.damageMin = damageMin;
+          this.damageMaw = damageMax;
+          //this.speed =
+      },
+
+
+    },
+    potions :{
+      life: function() {
+        this.name = 'Potion of life';
+        this.powerMin = 10;
+        this.powerMax = 50;
+        //this.cooldown =
+      },
+    },
   }
 };
 
