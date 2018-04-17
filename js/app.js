3da815ac1aba171223b7ad507c13ded5c4f7c75a
Arknoid <arknoid@ArchArknoid.localdomain>
2018-04-17 22:37:04 +0200

    add 1d6 toHit

--- js/app.js
+++ js/app.js
@@ -2,47 +2,43 @@ var app = {
 
   //tableau a remplir d'ennemies grace a createEnemysPool();
   enemyPool: [],
-  numberEnemy : 50,
+  numberEnemy: 5,
 
 
   init: function() {
-    // //Apelle du constructeur du joueur  avec un objet contenan  "nom,points de vie,force,dexterité,inteligence,or"
-    // app.player = app.createPlayer(app.arknoid);
-    //
-    // //Generation d'ennemies
-    // app.createEnemysPool(5);
-
     app.start();
 
   },
 
   //Demarage du jeu !
   start: function() {
-
+    // debugger;
     //Apelle du constructeur du joueur  avec un objet contenan  "nom,points de vie,force,dexterité,inteligence,or"
     app.player = app.createPlayer(app.player.arknoid);
 
     //Generation d'ennemies
     app.createEnemysPool(app.numberEnemy);
 
-    //Pour chaque enemies generer le joueur doit les combattres les uns apres les autres (pour le moment)
+    //Pour chaques enemies generer le joueur doit les combattres les uns apres les autres
     app.enemyPool.forEach(function(enemy) {
       while (enemy.life > 0 && app.player.life > 0) {
         app.player.combat(enemy);
-        app.player.showInventory();
       };
     });
     if (app.player.life > 0) {
       app.win();
+      app.player.showInventory();
     } else
-    app.gameOver();
+      app.gameOver();
   },
 
   //Generateur de nombres aleatoire arondie
   randomNumber: function(min = 0, max = 0) {
     return Math.round(Math.random() * (max - min) + min);
   },
-
+  setPercentage: function(number, percentage) {
+    return Math.round(number * percentage / 100);
+  },
   //Prototype de Base ! qui peu evoluer vers 'player' ou  'enemy' grace au systeme d'heritage;
   Character: function(obj) {
     this.name = obj.name;
@@ -51,50 +47,61 @@ var app = {
     this.dexterity = obj.dexterity;
     this.intelligence = obj.intelligence;
     this.gold = obj.gold;
+    //Calcules de compétence secondaire
+    this.attackBonus = app.setPercentage(this.strength, 20);
+    this.dodge = app.setPercentage(this.dexterity, 50);
+
+
     this.describ = function() {
       console.log("Je suis " + this.name + ' et j\'ai ' + this.life + ' de point de vies');
     };
+
     // Attaque un enemie cible
     this.attack = function(target) {
-      if (this.life > 0) {
-        //les degats sont calculer avec la force + un nombre aleatoir suivant la puissance min et max de l'arme du porteur
-        var damage = this.strength + app.randomNumber(this.weapon.damageMin, this.weapon.damageMax);
+      if (app.randomNumber(1,6) > this.weapon.toHit ) {
+
+        var damage = this.attackBonus + app.randomNumber(this.weapon.damageMin, this.weapon.damageMax);
+
         console.log(this.name + " attaque " + target.name + " et lui fait " + damage + " points de dégâts");
         target.life = target.life - damage;
+
         if (target.life > 0) {
           console.log(target.name + " a encore " + target.life + " points de vie");
         } else {
           target.life = 0;
           console.error(target.name + " est mort !");
         }
+      }else {
+        console.warn(this.name +" loupe son attaque");
       }
     }
   },
 
   //Prototype pour les ennemies
   Enemy: function(obj) {
+    //appel du construceur de Character
     app.Character.call(this, obj);
     this.valueXp = obj.valueXp;
-    this.weapon = new app.items.weapons.Arms(obj.arms.name, obj.arms.damageMin, obj.arms.damageMax);
+    this.weapon = new app.weapons.Arms(obj.arms.name, obj.arms.damageMin, obj.arms.damageMax ,obj.arms.toHit);
     //petite presentation pour debug
     this.describ = function() {
       console.log("je suis " + this.name + ', j\'ai ' + this.life + ' de vies  et ' + this.strength + ' de strength.');
     };
   },
 
-  //Prototype de creation du joueur
+  //Prototype du joueur
   Player: function(obj) {
     //Apelle du construteur du Prototype de base 'Charactere'
     app.Character.call(this, obj);
     //Plus des proprietés spécifique
     this.xp = 0;
-    this.key = 0;
+
     // Arme de base choisie dans le Namespace items.weapons
-    this.weapon = new app.items.weapons.Arms(obj.arms.name, obj.arms.damageMin, obj.arms.damageMax);
+    this.weapon = new app.weapons.Arms(obj.arms.name, obj.arms.damageMin, obj.arms.damageMax, obj.arms.toHit);
 
     //Affiche inventaire pour debug
     this.showInventory = function() {
-      console.warn('Inventory : ' + this.gold + " gold " + this.key + ' key ' + this.xp + ' experiences ' + this.life + ' lifes ' + 'arms : ' + this.weapon.name);
+      console.warn('Inventory : ' + this.gold + " gold "  + this.xp + ' experiences ' + this.life + ' lifes ' + 'arms : ' + this.weapon.name);
     };
 
     this.combat = function(opponent) {
@@ -102,10 +109,9 @@ var app = {
       this.attack(opponent);
       opponent.attack(this);
       if (opponent.life === 0) {
-        console.log(this.name + " a tué " + opponent.name + " gagne " +
-          opponent.valeur + " points d'expérience" + ' et  ' + opponent.gold + ' d\'or');
         this.xp += opponent.valueXp;
         this.gold += opponent.gold;
+        console.warn(this.name + " a tué " + opponent.name + " et a gagné : " + opponent.valueXp + 'Xp et ' + opponent.gold + ' Or ');
       }
     };
   },
@@ -116,10 +122,10 @@ var app = {
     return player;
   },
 
-  //Fonction pour crée un enemie  basé sur le Prototype charactere -> Enemy (!!!Voué a etre remplacer dans le DOM!! )
+  //Fonction pour crée un enemie  basé sur le Prototype charactere -> Enemy
   createEnemy: function(obj) {
-    var ennemie = new app.Enemy(obj);
-    return ennemie;
+    var enemy = new app.Enemy(obj);
+    return enemy;
   },
 
   //Permet de definir le nombre d'ennemies en lice
@@ -129,12 +135,12 @@ var app = {
       switch (randEnemy = app.randomNumber(1, 6)) {
         case 1:
         case 2:
-          app.enemyPool.push(app.createEnemy(app.enemys.orc));
+          app.enemyPool.push(app.createEnemy(app.enemys.goblins));
           break;
         case 3:
         case 4:
         case 5:
-          app.enemyPool.push(app.createEnemy(app.enemys.goblins));
+          app.enemyPool.push(app.createEnemy(app.enemys.orc));
           break;
         case 6:
           app.enemyPool.push(app.createEnemy(app.enemys.troll));
@@ -149,14 +155,14 @@ var app = {
     console.error("Game Over");
   },
   win: function() {
-  console.error("You win");
+    console.error("You win");
   },
   //NameSpace pour le joueur
   player: {
     arknoid: {
       name: 'arknoid',
-      life: 2000,
-      strength: 5,
+      life: 50,
+      strength: 20,
       dexterity: 15,
       intelligence: 20,
       gold: 100,
@@ -164,17 +170,18 @@ var app = {
         name: 'Sword',
         damageMin: 4,
         damageMax: 8,
+        toHit : 3,
       }
     },
   },
 
   //NameSpace pour les ennemies
   enemys: {
-    //enemies de base pour les testes
+    //enemies de base pour les tests
     orc: {
       name: 'orc',
-      life: 5,
-      strength: 6,
+      life: 10,
+      strength: 16,
       dexterity: 10,
       intelligence: 5,
       gold: 10,
@@ -183,13 +190,14 @@ var app = {
         name: 'Axe',
         damageMin: 1,
         damageMax: 6,
-      }
+        toHit: 3,
+      },
     },
 
     goblins: {
-      name: 'goblins',
-      life: 4,
-      strength: 4,
+      name: 'goblin',
+      life: 6,
+      strength: 10,
       dexterity: 15,
       intelligence: 8,
       gold: 20,
@@ -198,35 +206,37 @@ var app = {
         name: 'Rusty sword',
         damageMin: 1,
         damageMax: 3,
-      }
+        toHit: 2,
+      },
     },
 
     troll: {
       name: 'troll',
-      life: 10,
-      strength: 10,
+      life: 20,
+      strength: 25,
       dexterity: 5,
       intelligence: 2,
       gold: 100,
       valueXp: 20,
       arms: {
         name: 'Mace',
-        damageMin: 5,
+        damageMin: 10,
         damageMax: 15,
-      }
+        toHit: 5,
+      },
     },
   },
-  //NameSpace pour les items
-  items: {
-    weapons: {
-      Arms: function(name, damageMin, damageMax) {
-        this.name = name;
-        this.damageMin = damageMin;
-        this.damageMaw = damageMax;
-        //this.speed =
-      },
-
+  weapons: {
+    //Constructeur
+    Arms: function(name, damageMin, damageMax, toHit) {
+      this.name = name;
+      this.damageMin = damageMin;
+      this.damageMaw = damageMax;
+      this.toHit = toHit
+      //this.speed =
     },
+  },
+  items: {
     potions: {
       life: function() {
         this.name = 'Potion of life';
@@ -235,7 +245,8 @@ var app = {
         //this.cooldown =
       },
     },
-  }
+  },
+
 };
 
 
